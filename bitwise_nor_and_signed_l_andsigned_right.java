c77a1fd1fc38bf08d5eb0b807acc705344c3374c
sanskarsahu282002 <116749721+sanskarsahu282002@users.noreply.github.com>
2024-01-11 11:57:58 +0530

    Add files via upload

---
+++ bitwise_nor_and_signed_l_andsigned_right.java
@@ -0,0 +1,17 @@
+public class bitwise_nor_and_signed_l_andsigned_right {
+    public static void main(String[] args) {
+        int x=33;
+        //int x=-10;
+        //System.out.println(~x);
+        //System.out.println(x<<1);
+        //System.out.println(x<<2);
+        // int y=4;
+        // System.out.println(x<<y);
+        //System.out.println(x>>1);
+        //System.out.println(x>>2);
+        System.out.println(x>>>1);
+        System.out.println(x>>>2);
+        System.out.println("bitwise_nor_and_signed_l_andsigned_right.main()");
+
+    }
+}
