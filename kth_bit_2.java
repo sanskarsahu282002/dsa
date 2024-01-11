376fd708353ff5d113e5b2b5ce06614ff60b7e4e
sanskarsahu282002 <116749721+sanskarsahu282002@users.noreply.github.com>
2024-01-11 12:52:14 +0530

    Add files via upload

---
+++ kth_bit_2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class kth_bit_2 {
+    public static  int kth(int n,int k)
+    {
+        int x=1;
+        for(int i=0;i<(k-1);i++)
+        {
+            n=n/2;
+        }
+        if((x & n) !=0)
+        {
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+        return x;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter n");
+        int n = sc.nextInt();
+        System.out.println("enter the k");
+        int k = sc.nextInt();
+        kth(n, k);
+        System.out.println(kth(n, k));
+    }
+}
